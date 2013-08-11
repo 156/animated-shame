7ee4d1862ff131aff7b8e9b9da07de89126162fd
The Infinity Network <chaosmagick@zoho.com>
2013-08-11 11:53:09 -0700

    new file:   knowyourheadlines.pl

---
+++ knowyourheadlines.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+#
+# absolutely no warranty do not use.
+#
+
+use Mojo::UserAgent;
+use Mojo::Util 'encode';
+
+use Lingua::EN::Tagger;
+
+my %ignore = ();
+
+my $ua = Mojo::UserAgent->new;
+
+my ($pnum) = @ARGV or die "$0 <pages>";
+
+my $titles = '';
+
+
+$pnum++;
+
+for (my $i=1;$i<$pnum;$i++)
+{
+	my $search = "http://knowyoururl/page/$i/";
+
+	my $tx = $ua->get($search);
+
+	for my $e ($tx->res->dom('*')->each)
+	{
+		if ($e->type eq 'a')
+		{
+			if ($e->{class} eq 'newsfeed-title')
+			{
+				print $e->{title} . "\n";
+			}
+		}
+	}
+}
